de85d7ed4e3189c4b1cacc8696a1dc0dfdef14be
Alexander Smolin <88573504+emptybutton@users.noreply.github.com>
2025-06-22 16:40:09 +0700

    ref(`aiogram_dev`): use the command target

--- src/ttt/main/aiogram_dev/__main__.py
+++ src/ttt/main/aiogram_dev/__main__.py
@@ -1,17 +1,8 @@
-import asyncio
-
 from watchfiles import run_process
-
-from ttt.main.aiogram.di import container
-from ttt.main.aiogram.start_aiogram import start_aiogram
-
-
-def run_aiogram() -> None:
-    asyncio.run(start_aiogram(container))
 
 
 def main() -> None:
-    run_process("./src", target=run_aiogram)
+    run_process("./src", target="ttt")
 
 
 if __name__ == "__main__":
