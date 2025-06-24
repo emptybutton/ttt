172097f5d807c2887677df587796fb15c8513746
Alexander Smolin <88573504+emptybutton@users.noreply.github.com>
2025-06-24 17:49:54 +0700

    chore: complete `prod` `deploy`

--- deploy/prod/deploy.sh
+++ deploy/prod/deploy.sh
@@ -5,6 +5,7 @@ prefix=./deploy/prod
 main=${prefix}/docker-compose.yaml
 postgres_bootstrap=${prefix}/docker-compose.postgres-bootstrap.yaml
 
+docker compose -f ${main} build
 docker compose -f ${main} rm -sf
 
 docker compose -f ${postgres_bootstrap} up -d --wait
