cd1772ff75948128e6b22625375a4653527fde46
Alexander Smolin <88573504+emptybutton@users.noreply.github.com>
2025-07-02 13:53:07 +0700

    feat(`ttt_bot`): fix `/cancel_game` description

--- src/ttt/presentation/aiogram/common/bots.py
+++ src/ttt/presentation/aiogram/common/bots.py
@@ -4,7 +4,7 @@ from aiogram.types import BotCommand
 
 async def ttt_bot(bot: Bot) -> None:
     await bot.set_my_commands([
-        BotCommand(command="game", description="Начать поиск игры"),
+        BotCommand(command="game", description="Начать игру"),
         BotCommand(command="cancel_game", description="Отменить игру"),
         BotCommand(command="profile", description="Показать профиль"),
         BotCommand(command="select_emoji", description="Выбрать эмоджи"),
