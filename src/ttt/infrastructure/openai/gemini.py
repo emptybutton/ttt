4e5f5b8250dc5fa55ebff47cf03059833f5c31f2
Alexander Smolin <88573504+emptybutton@users.noreply.github.com>
2025-07-18 12:23:55 +0700

    fix(`gemini`): fix `mypy` & `ruff` errors

--- src/ttt/infrastructure/openai/gemini.py
+++ src/ttt/infrastructure/openai/gemini.py
@@ -1,6 +1,8 @@
+from openai import OpenAI
+
+
 def gemini(api_key: str, base_url: str) -> OpenAI:
     return OpenAI(
         api_key=api_key,
         base_url=f"{base_url}/v1beta/openai/",
-
     )
