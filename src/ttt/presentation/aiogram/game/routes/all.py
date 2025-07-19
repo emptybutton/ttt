f214b0d50af6a9a7f9af28e73ed4e1676f19fef5
Alexander Smolin <88573504+emptybutton@users.noreply.github.com>
2025-07-19 17:01:40 +0700

    ref(`game`): make dir for `game` usecase

--- src/ttt/presentation/aiogram/game/routes/all.py
+++ src/ttt/presentation/aiogram/game/routes/all.py
@@ -1,14 +1,16 @@
-from ttt.presentation.aiogram.game.routes.cancel_game import cancel_game_router
+from ttt.presentation.aiogram.game.routes.game.cancel_game import (
+    cancel_game_router,
+)
+from ttt.presentation.aiogram.game.routes.game.make_move_in_game import (
+    make_move_in_game_router,
+)
+from ttt.presentation.aiogram.game.routes.game.wait_game import wait_game_router
 from ttt.presentation.aiogram.game.routes.game_with_ai.start_game_with_ai import (  # noqa: E501
     start_game_with_ai_router,
 )
 from ttt.presentation.aiogram.game.routes.game_with_ai.wait_ai_type_to_start_game_with_ai import (  # noqa: E501
     wait_ai_type_to_start_game_with_ai_router,
 )
-from ttt.presentation.aiogram.game.routes.make_move_in_game import (
-    make_move_in_game_router,
-)
-from ttt.presentation.aiogram.game.routes.wait_game import wait_game_router
 
 
 game_routers = (
