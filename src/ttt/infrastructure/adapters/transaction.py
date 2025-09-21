a1d4ea8cb28d55204f824c74799c906d4e242fb9
Alexander Smolin <88573504+emptybutton@users.noreply.github.com>
2025-09-21 20:55:48 +0700

    fix(`adapters`): implement new `transaction`s (#62)

--- src/ttt/infrastructure/adapters/transaction.py
+++ src/ttt/infrastructure/adapters/transaction.py
@@ -1,34 +1,29 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from types import TracebackType
 from typing import Self
 
-from sqlalchemy.ext.asyncio import AsyncSession, AsyncSessionTransaction
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from ttt.application.common.ports.transaction import Transaction
-from ttt.entities.tools.assertion import not_none
+from ttt.application.common.ports.transaction import (
+    NotSerializableTransaction,
+    ReadonlyTransaction,
+    SerializableTransaction,
+)
+from ttt.entities.tools.assertion import assert_, not_none
+from ttt.infrastructure.sqlalchemy.serialization import (
+    reraise_serialization_error,
+)
 
 
 @dataclass
-class InPostgresTransaction(Transaction):
+class InPostgresSerializableTransaction(SerializableTransaction):
     _session: AsyncSession
-    _transaction: AsyncSessionTransaction | None = field(
-        init=False,
-        default=None,
-    )
-    _nesting_counter: int = field(
-        init=False,
-        default=0,
-    )
 
     async def __aenter__(self) -> Self:
-        self._nesting_counter += 1
-
-        if self._transaction is None:
-            self._transaction = await self._session.begin()
-        elif not self._transaction.is_active:
-            await self._session.rollback()
-            self._transaction = await self._session.begin()
-
+        assert_(not self._session.in_transaction())
+        await self._session.connection(
+            execution_options={"isolation_level": "SERIALIZABLE"},
+        )
         return self
 
     async def __aexit__(
@@ -37,10 +32,56 @@ class InPostgresTransaction(Transaction):
         error: BaseException | None,
         traceback: TracebackType | None,
     ) -> None:
-        self._nesting_counter -= 1
-
-        if self._nesting_counter == 0:
-            transaction = not_none(self._transaction)
+        transaction = not_none(self._session.get_transaction())
+        with reraise_serialization_error():
             await transaction.__aexit__(error_type, error, traceback)
-            self._transaction = None
-            return
+
+    async def commit(self) -> None:
+        transaction = not_none(self._session.get_transaction())
+        with reraise_serialization_error():
+            await transaction.commit()
+
+
+@dataclass
+class InPostgresNotSerializableTransaction(NotSerializableTransaction):
+    _session: AsyncSession
+
+    async def __aenter__(self) -> Self:
+        assert_(not self._session.in_transaction())
+        await self._session.connection(
+            execution_options={"isolation_level": "READ COMMITED"},
+        )
+        return self
+
+    async def __aexit__(
+        self,
+        error_type: type[BaseException] | None,
+        error: BaseException | None,
+        traceback: TracebackType | None,
+    ) -> None:
+        transaction = not_none(self._session.get_transaction())
+        await transaction.__aexit__(error_type, error, traceback)
+
+    async def commit(self) -> None:
+        transaction = not_none(self._session.get_transaction())
+        await transaction.commit()
+
+
+@dataclass
+class InPostgresReadonlyTransaction(ReadonlyTransaction):
+    _session: AsyncSession
+
+    async def __aenter__(self) -> Self:
+        assert_(not self._session.in_transaction())
+        options = {"isolation_level": "SERIALIZABLE", "readonly": True}
+        await self._session.connection(execution_options=options)
+        return self
+
+    async def __aexit__(
+        self,
+        error_type: type[BaseException] | None,
+        error: BaseException | None,
+        traceback: TracebackType | None,
+    ) -> None:
+        transaction = not_none(self._session.get_transaction())
+        await transaction.__aexit__(error_type, error, traceback)
